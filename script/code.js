d084c85f05555a4e7608ed6e293006a9fd1aef8b
Chelsea-Julie <chelseaqjulie.gmail.com>
2024-05-23 10:24:38 +0200

    completed

---
+++ script/code.js
@@ -0,0 +1,38 @@
+// Question 1
+// - Create a function called "greet" that accepts a single parameter. Log the parameter along with a "Hello".
+
+function greet(params) {
+    console.log("Hello" + params);
+}
+
+greet(" John Doe")
+
+// Question 2
+// - Create a function called multiply. It should accept two parameters and log the multiplication of the two given parameters.
+
+function multiplication(numb1, numb2) {
+    console.log(numb1 * numb2);
+}
+
+multiplication(5, 6)
+
+// Question 3
+// - Create a function called tableOfFive which accepts a single parameter. Return the parameter multiplied by five to a variable, and log this variable.
+
+function tableOfFive(numb1) {
+    return numb1 * 5;
+}
+
+
+let answer = tableOfFive(4)
+console.log(answer);
+
+
+
+
+
+
+
+
+
+
